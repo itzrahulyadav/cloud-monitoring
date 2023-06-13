b9eac24c864f16afbb5d0303b594a26a1da54798
Rahul Yadav <rahulkyadav9691@gmail.com>
2023-06-13 20:38:47 +0000

    add python file

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    print("hello world")
     cpu_percent = psutil.cpu_percent()
-    mem_percent = psutil.virtual_memory()
+    mem_percent = psutil.virtual_memory().percent
     Message = None
     if cpu_percent > 80 or cpu_percent > 80:
-        Message = High cpu or memory utilization detected,please scale up""
+        Message = "High cpu or memory utilization detected,please scale up"
     return f"cpu utilization : {cpu_percent} and memory utilization : {mem_percent}"
 
     if __name__ == "__main__":
-        app.run(debug=True,host = '0.0.0.0')
+        app.run(debug=True,host=5000)
 
 
 
